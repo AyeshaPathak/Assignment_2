987509efb8d8b7b9e977f82ed3e9ec34d6486aee
AyeshaPathak <pathakayesha9@gmail.com>
2024-05-26 02:32:25 +0930

    BubbleTea class and attributes created and given private attribute formatting

--- bubble_tea.py
+++ bubble_tea.py
@@ -1,1 +1,10 @@
+class BubbleTea:
+  def __init__(self, drink_name, size, ice_level, sugar_level, tea_type, topping_name, price):
+    self.__drink_name = drink_name
+    self.__size = size 
+    self.__ice_level = ice_level
+    self.__sugar_level = sugar_level
+    self.__tea_type = tea_type
+    self.__topping_name = topping_name
+    self.__price = price 
 
