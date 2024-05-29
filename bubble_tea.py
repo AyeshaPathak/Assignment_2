80e58dfa174d009cadd70c8e74a9cd62d609041b
AyeshaPathak <pathakayesha9@gmail.com>
2024-05-29 10:48:56 +0930

    In this commit I have created a plan to connect classes and implement dictionaries. Next, I will work on adding more info to the dictionaries.

--- bubble_tea.py
+++ bubble_tea.py
@@ -1,4 +1,10 @@
-class BubbleTea:
+class TeaType:
+  def __init__(self):
+    # will hold the various types of teas someone can order (4 different dictionaries holding drink name and ingredients)
+    pass 
+
+
+class BubbleTea(TeaType): # then utilise the dictionaries in the methods in this class 
   def __init__(self, drink_name, size, ice_level, sugar_level, tea_type, topping_name, price):
     self.__drink_name = drink_name
     self.__size = size 
@@ -22,6 +28,7 @@ class BubbleTea:
     
   def add_topping(self, topping_name, drink_name):
     topping_list = ["Custard", "Mousse", "Pearls", "Cookie Crumb", "Mixed Jellies", "Herbal Jelly", "Coconut Jelly, Aloe Vera", "Mango Popping Pearls", "Strawberry Popping Pearls", "Apple Popping Pearls"] 
+# add dictionary up here 
 
     if topping_name in topping_list:
       extra_topping = input("This topping is already on this drink. Would you like to add extra? [Yes/No]")
@@ -34,7 +41,18 @@ class BubbleTea:
     elif topping_name not in topping_list:
       print(topping_name , "is not available as a topping for", drink_name)
 
+  def remove_topping(self):
+    milk_menu_dict = {"Green Tea Matcha Latte": ["Matcha", "Milk", "Green Tea Powder", "Ice"], "Classic Milk Tea": ["Sweetened Black Tea", "Milk", "Pearls"], "Cookie Crumble": ["Blended Oreo", "Milk", "Whipped Cream", "Cookie Crumb"], "Choco Blast": ["Chocolate Sauce", "Milk", "Shredded Chocolate"]} 
+    fruit_menu_dict = {}
+    sparkling_menu_dict = {}
+    hot_menu_dict = {}
+    # 3 milk teas, 3 fruit water based teas, 3 hot teas, 3 sparkling teas (different dictionaries)
+    pass
 
 
-bubble_tea = BubbleTea("Green Tea Matcha Latte", "medium", "low", "medium", "green tea", "Cookie Crumb", 10)
-bubble_tea.add_topping("Pearls", "Green Tea Matcha Latte")
+
+
+
+
+bubble_tea = BubbleTea("Green Tea and Matcha", "medium", "low", "medium", "green tea", "Cookie Crumb", 10)
+bubble_tea.add_topping("Pearls", "Green Tea and Matcha")
