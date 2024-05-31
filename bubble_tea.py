48a838187897140c51f6fb85dbb0b514dbad88fb
AyeshaPathak <pathakayesha9@gmail.com>
2024-05-31 21:08:25 +0930

    In this commit i have created a layout of the areas for an abstract class. Next, I will be working on creating dictionaries.

--- bubble_tea.py
+++ bubble_tea.py
@@ -1,10 +1,13 @@
+from abc import ABC, abstractmethod
+
+
 class TeaType:
   def __init__(self):
     # will hold the various types of teas someone can order (4 different dictionaries holding drink name and ingredients)
     pass 
 
 
-class BubbleTea(TeaType): # then utilise the dictionaries in the methods in this class 
+class BubbleTea(ABC, TeaType): # then utilise the dictionaries in the methods in this class 
   def __init__(self, drink_name, size, ice_level, sugar_level, tea_type, topping_name, price):
     self.__drink_name = drink_name
     self.__size = size 
@@ -28,8 +31,6 @@ class BubbleTea(TeaType): # then utilise the dictionaries in the methods in this
     
   def add_topping(self, topping_name, drink_name):
     topping_list = ["Custard", "Mousse", "Pearls", "Cookie Crumb", "Mixed Jellies", "Herbal Jelly", "Coconut Jelly, Aloe Vera", "Mango Popping Pearls", "Strawberry Popping Pearls", "Apple Popping Pearls"] 
-# add dictionary up here 
-
     if topping_name in topping_list:
       extra_topping = input("This topping is already on this drink. Would you like to add extra? [Yes/No]")
       while extra_topping not in ("Yes", "No"):
@@ -41,14 +42,26 @@ class BubbleTea(TeaType): # then utilise the dictionaries in the methods in this
     elif topping_name not in topping_list:
       print(topping_name , "is not available as a topping for", drink_name)
 
-  def remove_topping(self):
-    milk_menu_dict = {"Green Tea Matcha Latte": ["Matcha", "Milk", "Green Tea Powder", "Ice"], "Classic Milk Tea": ["Sweetened Black Tea", "Milk", "Pearls"], "Cookie Crumble": ["Blended Oreo", "Milk", "Whipped Cream", "Cookie Crumb"], "Choco Blast": ["Chocolate Sauce", "Milk", "Shredded Chocolate"]} 
-    fruit_menu_dict = {}
-    sparkling_menu_dict = {}
-    hot_menu_dict = {}
-    # 3 milk teas, 3 fruit water based teas, 3 hot teas, 3 sparkling teas (different dictionaries)
+  @abstractmethod
+  def remove_topping(self, topping_name, drink_name):
     pass
 
+    
+
+
+class MilkFruit(BubbleTea):
+  def __init__(self):
+    pass
+
+
+class SparklingHot(BubbleTea):
+  def __init__(self):
+    pass
+
+
+class Frozen(BubbleTea):
+  def __init__(self):
+    pass
 
 
 
